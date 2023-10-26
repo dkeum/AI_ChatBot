9dd59f235f7fa162ccd7acb6237d89af883d78d1
dkeum <dkeum@sfu.ca>
2023-10-26 15:33:20 -0700

    update

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,39 +1,40 @@
 import { supabase } from "@/supabase/supabase";
 import AICard from "./components/AICard";
 
-
-
 export default async function ProductPage() {
+  let { data: chatBots, error } = await supabase
+    .from("chatBot")
+    .select("id,name,slug,description,img_url,content_id, isLocked");
 
-    
-    let { data: chatBots, error } = await supabase
-    .from('chatBot')
-    .select('id,name,slug,description,img_url,content_id, isLocked')
-
-if (error) {
+  if (error) {
     console.error("Error fetching chatBots:", error);
     return {
-    notFound: true, // Handle the error as needed
+      notFound: true, // Handle the error as needed
     };
-}
+  }
   // console.log(chatBots);
 
   return (
     <div className="bg-black h-screen w-full flex flex-col justify-center items-center align-middle ">
-     
       <div className="flex flex-col">
-        <h1 className="text-white font-bold text-3xl justify-center items-center text-center mb-10">Chat with AI Chatbots</h1> 
-      <div className="flex sm:flex-col sm:gap-y-5 gap-x-10 md:flex-row justify-center">
-      {chatBots && chatBots.map(item=>(
-            <AICard image={item.img_url} name={item.name} slug={item.slug} description={item.description} key={item.id} id={item.content_id} isLocked={item.isLocked}/>
-         
-      ))}
-      </div>
-
+        <h1 className="text-white font-bold text-3xl justify-center items-center text-center mb-10">
+          Chat with AI Chatbots
+        </h1>
+        <div className="flex sm:flex-col sm:gap-y-5 gap-x-10 md:flex-row justify-center">
+          {chatBots &&
+            chatBots.map((item) => (
+              <AICard
+                image={item.img_url}
+                name={item.name}
+                slug={item.slug}
+                description={item.description}
+                key={item.id}
+                id={item.content_id}
+                isLocked={item.isLocked}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
 }
-
-
-
