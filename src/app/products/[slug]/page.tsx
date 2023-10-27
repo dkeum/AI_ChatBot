2c544243531474145f572ec80872737011c253fe
dkeum <dkeum@sfu.ca>
2023-10-26 21:34:45 -0700

    updating ui

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { createClient } from "@supabase/supabase-js";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function AIChatbot({ params }: { params: { slug: string } }) {
- 
- 
-    const searchParams = useSearchParams();
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const supabase = createClientComponentClient();
+
+  const searchParams = useSearchParams();
   const id = searchParams.get("Id");
   const [chatsrc, setChatsrc] = useState(
     "https://www.stack-ai.com/embed/ae575f46-44ce-4241-909b-af2a76c5b9cb/c04bed43-c4df-4f99-a2b7-c7831087b75b/6531eadd42fd281cfc0e3208"
@@ -15,22 +21,30 @@ export default function AIChatbot({ params }: { params: { slug: string } }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
-  );
-
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const { data: chatBot_profile } = await supabase
+          .from("chatBot")
+          .select("name,img_url,description,category")
+          //@ts-ignore
+          .eq("content_id", parseInt(id)).single();
+
+          console.log("this is the chatBot_profile")
+          console.log(chatBot_profile);
+          setName(chatBot_profile.name); 
+          setImage(chatBot_profile.img_url);
+          setDescription(chatBot_profile.description)
+          setCategory(chatBot_profile.category);
+
         const { data: chatBot_content, error } = await supabase
           .from("chatBot_content")
           .select("chatbot_url")
-           //@ts-ignore
+          //@ts-ignore
           .eq("id", parseInt(id));
 
         if (error) {
-            //@ts-ignore
+          //@ts-ignore
           setError("Error fetching data");
         } else if (chatBot_content && chatBot_content.length > 0) {
           setChatsrc(chatBot_content[0].chatbot_url);
@@ -38,24 +52,34 @@ export default function AIChatbot({ params }: { params: { slug: string } }) {
 
         setLoading(false);
       } catch (error) {
-         //@ts-ignore
+        //@ts-ignore
         setError("Error fetching data: " + error.message);
         setLoading(false);
       }
     };
 
     fetchData();
-  }, [id]);
+  }, []);
 
   return (
     <div className="bg-black h-screen w-full flex sm:flex-col md:flex-row items-center justify-center relative">
-       <h1 className="text-white text-3xl font-bold absolute top-28"> Chat with {params.slug} </h1>
+
+
+    
+      <div className="text-white text-3xl flex flex-col gap-y-10">
+        <h1 className="text-white text-5xl font-bold mb-10 mr-10">Chat with <br/>{name}</h1>
+        <Image src={image} alt={params.slug} width={300} height={300} className=""/>
+        <h1 className="text-white">{description}</h1>
+        <h1 className="text-white">Expert in {category}</h1>
+
+      </div>
+      <div className="w-[400px] h-[620px] ms-5">
       {loading ? (
         <p>Loading...</p>
       ) : error ? (
         <p>{error}</p>
       ) : (
-         
+        <>
           <iframe
             src={chatsrc}
             width="400px"
@@ -72,8 +96,9 @@ export default function AIChatbot({ params }: { params: { slug: string } }) {
               borderRadius: "10px",
             }}
           />
-        
+        </>
       )}
+      </div>
     </div>
   );
 }
--- src/app/products/components/AICard.tsx
+++ src/app/products/components/AICard.tsx
@@ -20,12 +20,7 @@ export default function AICard({image,name,slug,description,className,id,isLocke
         <div className={twMerge("max-w-sm w-[250px] lg:max-w-full lg:flex ", className)}>
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal ">
                 <div className="mb-8">
-                {isLocked && <p className="text-sm text-gray-600 flex items-center">
-                    <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                    </svg>
-                    Members only
-                </p>} 
+               
                 <div className="h-48 w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center object-none overflow-hidden">
                     <Image src={image} alt={name} width={192} height={192}/>
                 </div>
@@ -37,6 +32,12 @@ export default function AICard({image,name,slug,description,className,id,isLocke
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ">Chat with me</button>
                     </Link>
                     }
+                     {isLocked && <p className="text-sm text-gray-600 flex items-center">
+                    <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                    </svg>
+                    Members only
+                </p>} 
                 </div>
             </div>
         </div>
